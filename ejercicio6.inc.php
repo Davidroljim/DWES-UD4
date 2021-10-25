89fadaf4dfdf36fb9d97f81d68f185616b53c96b
Davidroljim <davidroldan2901@gmail.com>
2021-10-25 20:29:34 +0200

    terminado

--- ejercicio6.inc.php
+++ ejercicio6.inc.php
@@ -1,23 +1,99 @@
 <?php
 
 function creaConexion (){
-    @$mysqli= mysqli_connect('localhost','developer','developer','agenciaviajes');
+    $mysqli= mysqli_connect('localhost','developer','developer','agenciaviajes');
     $error = mysqli_connect_errno($mysqli);
     if ($error!=null) {
         echo "<p>Error $error conectando a la base de datos:",mysqli_connect_error(),"</p>";
         exit();
+    }else {
+        echo "La conexión se ha establecido correctamente";
+        echo "<br>";
+        return $mysqli;
+    }
+    
+}
+
+function creaVuelo ($origen,$destino,$fecha,$companya,$modeloAvion){
+    $mysqli = creaConexion();
+    $sql="INSERT INTO `vuelos` (Origen, Destino, Fecha, Companya, ModeloAvion) VALUES (?, ?, ?, ?, ?)";
+    $consulta = mysqli_stmt_init($mysqli);
+
+    if ($stmt= mysqli_prepare($mysqli, $sql)) {
+        
+        mysqli_stmt_bind_param($stmt, "sssss", $origen, $destino, $fecha, $companya, $modeloAvion);
+        mysqli_stmt_execute($stmt);
+
+        mysqli_stmt_close($stmt);
+    }
+        echo "vuelo creado";
+        echo "<br>";
+}
+
+function modificaDestino ($nuevoDestino,$id){
+    $mysqli = creaConexion();
+    $sql = "UPDATE `vuelos` SET `Destino`=? WHERE `id`=?";
+    $consulta = mysqli_stmt_init($mysqli);
+
+    if ($stmt= mysqli_prepare($mysqli, $sql)) {
+        
+        mysqli_stmt_bind_param($stmt, "si", $nuevoDestino, $id);
+        mysqli_stmt_execute($stmt);
+
+        mysqli_stmt_close($stmt);
+    }
+        echo "vuelo modificado";
+        echo "<br>";
+}
+
+function modificaCompanya ($nuevaCompanya,$id){
+    $mysqli = creaConexion();
+    $sql = "UPDATE `vuelos` SET `Companya`=? WHERE `id`=?";
+    $consulta = mysqli_stmt_init($mysqli);
+
+    if ($stmt= mysqli_prepare($mysqli, $sql)) {
+        
+        mysqli_stmt_bind_param($stmt, "si", $nuevaCompanya, $id);
+        mysqli_stmt_execute($stmt);
+
+        mysqli_stmt_close($stmt);
+    }
+        echo "vuelo modificado";
+        echo "<br>";
+}
+
+function eliminaVuelo ($id){
+
+    $mysqli = creaConexion();
+    $sql = "DELETE FROM `vuelos` WHERE `id`=? ";
+    $consulta = mysqli_stmt_init($mysqli);
+
+    if ($stmt= mysqli_prepare($mysqli, $sql)) {
+        
+        mysqli_stmt_bind_param($stmt, "i", $id);
+        mysqli_stmt_execute($stmt);
+
+        mysqli_stmt_close($stmt);
+        echo "vuelo BORRADO";
+        echo "<br>";
     }
 }
 
-function creaVuelo (){
-    $result = mysqli_query($mysqli, "INSERT INTO `vuelos` (Origen, Destino, Fecha, Companya, ModeloAvion) VALUES ('Madrid', 'Valencia', '2021-10-21 09:16:52', 'IBERIA', 'A350')");
-    if ($result==false) {
-        echo "La consulta no ha funcionado correctamente";
-    }else {
-        echo "Se han insertado ", mysqli_affected_rows($mysqli), " filas.";
-        echo "<br>";
-        //muestra el número de filas afectadas
-        echo "El id del último elemento añadido es ", mysqli_insert_id($mysqli);
-}
+function extraeVuelos (){
+    $mysqli = creaConexion();
+    $sql = "SELECT * FROM vuelos";
+    $consulta = mysqli_stmt_init($mysqli);
 
+    if ($stmt= mysqli_prepare($mysqli, $sql)) {
+        mysqli_stmt_execute($stmt);
+        
+        mysqli_stmt_bind_result($stmt, $id, $origen, $destino, $fecha, $companya, $modeloAvion);
+        
+        while (mysqli_stmt_fetch($stmt)) {
+            print "El vuelo con origen $origen y destino $destino tiene fecha prevista $fecha y es operado por la compañía $companya con el modelo de avión $modeloAvion";
+            echo "<br>";echo "<br>";
+        }
+        mysqli_stmt_close($stmt);
+    }
+}
 ?>
--- ejercicio6.php
+++ ejercicio6.php
@@ -10,7 +10,13 @@
     
 <?php
     include "ejercicio6.inc.php";
-    
+    creaConexion();
+    //creaVuelo("Madrid","Barcelona","2021-10-28 14:04:59","IBERIA","Airbus");
+    //modificaDestino("Sevilla","6");
+    //modificaCompanya("Vueling","6");
+    //eliminaVuelo("11");
+    extraeVuelos();
+    mysqli_close($mysqli);
 ?>
 
 </body>
